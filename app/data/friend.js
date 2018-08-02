2f9fadb13afdcc326f04744384a4400fb4b5986e
Marshall Berry <marshallgberry@gmail.com>
2018-08-02 16:42:37 -0400

    Updated friend.js

---
+++ app/data/friend.js
@@ -0,0 +1,86 @@
+var friendsArray = [{
+    "name": "Godfrey",
+    "photo": "https://i.chzbgr.com/full/6578206208/hD90C5E9B/",
+    "scores": [
+        
+        2,
+        3,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2
+    ]
+},
+{
+    "name": "Alf",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU3MM2xEHu--aRGnwiGSFt32m4P2XgX0SdKKFCbMpggkQuYcdvHQ",
+    "scores": [
+        
+        3,
+        3,
+        1,
+        5,
+        2,
+        3,
+        2,
+        2,
+        4,
+        5
+    ]
+},
+{
+    "name": "Felicia",
+    "photo": "http://cdn.ebaumsworld.com/mediaFiles/picture/604025/84611401.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+},
+{
+    "name": "Becky",
+    "photo": "http://www.teamjimmyjoe.com/wp-content/uploads/2013/11/Bad-Hair-Bump.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+
+{
+    "name": "Dan",
+    "photo": "http://www.chilloutpoint.com/images/2010/03/wtf-famili-photos/weird-family-photographs-04.jpg",
+    "scores": [
+        4,
+        2,
+        1,
+        3,
+        1,
+        5,
+        3,
+        1,
+        4,
+        3
+    ]
+}
+];
+
+module.exports = friendsArray;
